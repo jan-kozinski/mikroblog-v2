6b6c682978022042f6d2bc0b42881301cf2954a2
Jan Kozinski <dzejdzej9753@gmail.com>
2021-12-19 12:00:29 +0100

    fixed production build error

--- src/adapters/express-adapter/index.js
+++ src/adapters/express-adapter/index.js
@@ -46,7 +46,7 @@ export default async function start(callback) {
     app.use(express.static("client/build"));
 
     app.get("*", (req, res) =>
-      res.sendFile(path.resolve(__dirname, "./client/build/index.html"))
+      res.sendFile(path.resolve(__dirname, "../../../client/build/index.html"))
     );
   }
 
